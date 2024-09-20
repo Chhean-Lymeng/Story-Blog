030adca4c1d8a75687f639534e0d4504a63cd484
Chhean-Lymeng <chheanlymeng@gmail.com>
2024-09-20 15:17:06 +0700

    s

--- resources/views/frontend/website/single_news.blade.php
+++ resources/views/frontend/website/single_news.blade.php
@@ -67,14 +67,6 @@
                             <small><a href="#" title="">by {{ $user->name }}</a></small>
                             <small><a href="#" title=""><i class="fa fa-eye"></i> {{ $news->views }}</a></small>
                         </div>
-
-                        <div class="post-sharing">
-                            <ul class="list-inline">
-                                <li><a href="#" class="fb-button btn btn-primary"><i class="fa fa-facebook"></i> <span class="d-none d-md-inline">Share on Facebook</span></a></li>
-                                <li><a href="#" class="tw-button btn btn-primary"><i class="fa fa-twitter"></i> <span class="d-none d-md-inline">Tweet on Twitter</span></a></li>
-                                <li><a href="#" class="gp-button btn btn-primary"><i class="fa fa-google-plus"></i></a></li>
-                            </ul>
-                        </div>
                     </div>
 
                     <div class="single-post-media">
